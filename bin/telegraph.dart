492ee7a614a9b6ad3ad85fd3783444e46f2cf0e8
Your Name <you@example.com>
2023-10-27 15:24:25 +0700

    update

--- bin/telegraph.dart
+++ bin/telegraph.dart
@@ -7,9 +7,10 @@ import "package:general_lib/general_lib.dart";
 void main(List<String> arguments) async {
   Telegraph telegraph = Telegraph();
 
-  var res = await telegraph.getAccountInfo(
-    access_token: ""
-  );
+  var res = await telegraph.createAccount(
+    short_name: "salsa",
+    author_name: "sas"
+   );
 
   res.printPretty();
 
